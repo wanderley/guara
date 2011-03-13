be28e68289deb92bb32972395fe7226314d341a5
Wanderley Guimarães <wanderley.guimaraes@gmail.com>
2011-03-13 19:48:38 -0300

    Improve testing of special judge

--- lib/test_sols.sh
+++ lib/test_sols.sh
@@ -3,8 +3,8 @@ log_path=$(mktemp "/tmp/XXXXXXXXXXX")
 
 time_limit=$(ruby -r 'yaml' -e "puts YAML::load(File.read('config.yml'))['time_limit']")
 special_judge=$(ruby -r 'yaml' -e "puts YAML::load(File.read('config.yml'))['special_judge']")
-if [ -e $special_judge ]; then
-  special_judge_cmd=$(sh $script_dir/compile.sh $special_judge)
+if [ -n "$special_judge" ]; then
+  special_judge_cmd=$(sh $scripts_dir/compile.sh $special_judge)
 fi
 
 ulimit -t $time_limit
@@ -17,7 +17,7 @@ for sol in $(ls sols/ | egrep "\-[0-9]+pts..{2,3}$" | egrep "(.c|.cpp|.rb|.py)")
   printf "%72s\n" $sol
   echo   "------------------------------------------------------------------------"
 
-  sol_cmd=$(sh $script_dir/compile.sh sols/$sol)
+  sol_cmd=$(sh $scripts_dir/compile.sh sols/$sol)
   for test_dir in $(ls tests); do
     passed=true
     printf "%-10s" "tests/$test_dir"
@@ -25,7 +25,7 @@ for sol in $(ls sols/ | egrep "\-[0-9]+pts..{2,3}$" | egrep "(.c|.cpp|.rb|.py)")
       output_file=$(echo $input_file | sed 's:in\(.*\)$:out\1:g')
       user_output_file=$(mktemp /tmp/XXXXXXXXXXX)
       $sol_cmd < $input_file > $user_output_file
-      if [ -e $special_judge ]; then
+      if [ -n "$special_judge" ]; then
         $special_judge_cmd $input_file $output_file $user_output_file sols/$sol > /dev/null
       else
         diff $output_file $user_output_file > /dev/null
