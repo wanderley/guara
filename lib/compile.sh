381aa5aabbc9bd542fdb8d9ee1b79e260ee9589d
Joel Silva Uchoa <joeluchoa@gmail.com>
2011-06-08 18:29:32 -0300

    Adiocinada extenção .c99 para compilar códigos com gcc -std=gnu99

--- lib/compile.sh
+++ lib/compile.sh
@@ -6,33 +6,39 @@ cmd=''
 ret_val=1
 
 case $source in
-	*.c)
-		t=`mktemp /tmp/XXXXXXXXXX`
-		gcc -O2 -fomit-frame-pointer -o $t $source
+  *.c)
+    t=`mktemp /tmp/XXXXXXXXXX`
+    gcc -O2 -fomit-frame-pointer -o $t $source
     ret_val=$?
-		cmd="$t"
-		;;
-	*.cpp)
-		t=`mktemp /tmp/XXXXXXXXXX`
-		g++ -O2 -fomit-frame-pointer -o $t $source
+    cmd="$t"
+    ;;
+  *.c99)
+    t=`mktemp /tmp/XXXXXXXXXX`
+    gcc -std=gnu99 -O2 -fomit-frame-pointer -o $t $source
     ret_val=$?
-		cmd="$t"
-		;;
-	*.py)
-		cmd="python $source"
-		;;
-	*.rb)
-		cmd="ruby $source"
-		;;
-	*.pl)
-		cmd="perl $source"
-		;;
-	*.pas)
-		t=`mktemp /tmp/XXXXXXXXXX`
-		fpc -O2 -o$t $source
+    cmd="$t"
+    ;;
+  *.cpp)
+    t=`mktemp /tmp/XXXXXXXXXX`
+    g++ -O2 -fomit-frame-pointer -o $t $source
     ret_val=$?
-		cmd="$t"
-		;;
+    cmd="$t"
+    ;;
+  *.py)
+    cmd="python $source"
+    ;;
+  *.rb)
+    cmd="ruby $source"
+    ;;
+  *.pl)
+    cmd="perl $source"
+    ;;
+  *.pas)
+    t=`mktemp /tmp/XXXXXXXXXX`
+    fpc -O2 -o$t $source
+    ret_val=$?
+    cmd="$t"
+    ;;
 esac
 
 echo $cmd
